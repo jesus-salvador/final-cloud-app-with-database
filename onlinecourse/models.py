47af842d6dd3facfe0b18774d99d8e61d16d58b6
Jesus Meza <jsmsmail@gmail.com>
2023-03-14 18:51:10 -0600

    score when selected all the right answers

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -114,7 +114,7 @@ class Question(models.Model):
         """ sample model method to calculate if learner get the score of the question"""
         all_answers = self.choice_set.filter(is_correct=True).count()
         selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-        if all_answers == selected_correct:
+        if all_answers == selected_correct and selected_correct == len(selected_ids):
             return True
         else:
             return False
